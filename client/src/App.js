1437004db75bc1c8c030101f34e14763ff6d2e4b
Adrian Li <li.adrianmc@gmail.com>
2018-07-04 03:56:14 -0400

    cleaned up unnecessary frontend code

--- client/src/App.js
+++ client/src/App.js
@@ -7,40 +7,22 @@ import contractDefinition from './contracts/MyToken.json'
 import './App.css'
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null }
+  state = { web3: null, accounts: null, contract: null }
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
       const web3 = await getWeb3()
-
-      // Use web3 to get the user's accounts.
       const accounts = await getAccounts(web3)
-
-      // Get the contract instance by passing in web3 and the contract definition.
       const contract = await getContractInstance(web3, contractDefinition)
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract }, this.runExample)
+      this.setState({ web3, accounts, contract })
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(`Failed to load web3, accounts, or contract. Check console for details.`)
       console.log(error)
     }
-  }
-
-  runExample = async () => {
-    const { accounts, contract } = this.state
-
-    // Stores a given value, 5 by default.
-    // await contract.set(5, { from: accounts[0] })
-    
-    // Get the value from the contract to prove it worked.
-    //const response = await contract.get.call({ from: accounts[0] })
-
-    // Update state with the result.
-   // this.setState({ storageValue: response.toNumber() })
   }
 
   buyBook = async () => {
@@ -53,9 +35,6 @@ class App extends Component {
     const { accounts, contract, web3 } = this.state
     const tx = await contract.balanceOf.call(accounts[0], { from: accounts[0] })
     console.log(tx.toString())
-    if(tx.toNumber() > 0) {
-      
-    }
   }
 
   render() {
@@ -65,7 +44,7 @@ class App extends Component {
     return (
       <div className="App">
         <button onClick={this.buyBook}>Click to Buy</button>
-        <button onClick={this.checkBalance}>Click to Verify</button>
+        <button onClick={this.checkBalance}>Check Balance of NFT</button>
       </div>
     );
   }
